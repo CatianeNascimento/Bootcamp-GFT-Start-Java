9ffc52b4d0d99cbd0cda6c4c7a8c42864543becf
CatianeNascimento <csnascimento07@gmail.com>
2022-05-08 12:43:07 -0300

    Exercicios aula 1 de Métodos

---
+++ Metodos/src/main/java/Metodos/Calculadora.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Metodos;
+
+/** Exercício da aula 1 de Métodos: Calcule as 4 operações básicas: soma, subtração,
+ * multiplicação e divisão. Sempre 2 valores tem que ser passados.
+ *
+ * @author Catiane
+ */
+public class Calculadora {
+    
+    public static void soma(double numero1, double numero2) {
+        
+        double resultado = numero1 + numero2;
+        
+        System.out.println("A soma de " + numero1 + " mais " + numero2 + " é "
+        + resultado);
+    }
+    
+    public static void subtracao(double numero1, double numero2) {
+        
+        double resultado = numero1 - numero2; 
+        
+        System.out.println("A subtração de " + numero1 + " menos " + numero2 
+        + " é " + resultado);
+    }
+    
+    public static void multuplicacao(double numero1,double numero2) {
+        
+        double resultado = numero1 * numero2;
+        
+        System.out.println("A multiplicação de " + numero1 + " vezes " + numero2
+        + " é " + resultado);
+    }
+    
+    public static void divisao(double numero1, double numero2) {
+        
+        double resultado = numero1 / numero2;
+        
+        System.out.println("A divisão de " + numero1 + " divido por " + numero2
+       + " é " +  resultado);
+    }
+}
---
+++ Metodos/src/main/java/Metodos/Main.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package Metodos;
+
+/** Classe principal dos exercícios da Aula 1 de Métodos.
+ *
+ * @author Catiane
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        //Calculadora 
+        System.out.println("** Exercício Calculadora ** ");
+        Calculadora.soma(7, 8); 
+        Calculadora.subtracao(9, 3);
+        Calculadora.multuplicacao(8, 7);
+        Calculadora.divisao(10, 2);   
+        
+        //Mensagem
+        System.out.println("**Exercício Mensagem **");
+        Mensagem.obterMensagem(8);
+        Mensagem.obterMensagem(13);
+        Mensagem.obterMensagem(24);
+        
+        //Empréstimo
+        System.out.println("** Exercício Emprestimo **");
+        Emprestimo.calcular(1000, Emprestimo.getDuasParcelas());
+        Emprestimo.calcular(1000, Emprestimo.getTresParcelas());
+        Emprestimo.calcular(1000, 5);
+    }
+       
+}
